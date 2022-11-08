9694ad74e057c4f72b4a93ec09c65357f91cb0fb
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-11-08 14:43:43 +0200

    Cube was animated with useFrame and useRef

--- src/Experience.js
+++ src/Experience.js
@@ -1,14 +1,26 @@
+import {useFrame} from "@react-three/fiber";
+import {useRef} from "react";
+
 export default function Experience (  ) {
+    const cubeRef = useRef();
+
+    useFrame((state, delta) => {
+        cubeRef.current.rotation.x += delta;
+        cubeRef.current.rotation.y += delta;
+    })
+
     return (
     <>
-        <mesh rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry scale={ 1.5 } />
             <meshBasicMaterial color='mediumpurple' />
         </mesh>
+
         <mesh position-x={ -2 } scale={ 1 }>
             <sphereGeometry />
             <meshBasicMaterial color='orange'/>
         </mesh>
+
         <mesh rotation-x={Math.PI / 2} position-y={ -2 } scale={ 10 }>
             <planeGeometry />
             <meshBasicMaterial color='greenyellow' wireframe />
