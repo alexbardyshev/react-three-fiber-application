0128119ba48d905fdb54b6c7b2fabd16ba66e5a7
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-11-08 11:53:25 +0200

    Sphere and plane were added to the scene

--- src/Experience.js
+++ src/Experience.js
@@ -2,8 +2,16 @@ export default function Experience (  ) {
     return (
     <>
         <mesh rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshBasicMaterial color='mediumpurple' wireframe />
+            <boxGeometry scale={ 1.5 } />
+            <meshBasicMaterial color='mediumpurple' />
+        </mesh>
+        <mesh position-x={ -2 } scale={ 1 }>
+            <sphereGeometry />
+            <meshBasicMaterial color='orange'/>
+        </mesh>
+        <mesh rotation-x={Math.PI / 2} position-y={ -2 } scale={ 10 }>
+            <planeGeometry />
+            <meshBasicMaterial color='greenyellow' wireframe />
         </mesh>
     </>
   )
