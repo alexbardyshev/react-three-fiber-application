ece7444cc87ff5984636a02ecf1a706d16015d3a
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-11-08 11:42:18 +0200

    Mesh was created and handled

--- src/Experience.js
+++ src/Experience.js
@@ -1,9 +1,9 @@
 export default function Experience (  ) {
     return (
     <>
-        <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
+        <mesh rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshBasicMaterial color='mediumpurple' wireframe />
         </mesh>
     </>
   )
