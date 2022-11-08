fe7f09c8e9972958d7cf217bf338a3f9fe82b2eb
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-11-08 17:30:45 +0200

    Cube and sphere were grouped

--- src/Experience.js
+++ src/Experience.js
@@ -11,15 +11,17 @@ export default function Experience (  ) {
 
     return (
     <>
-        <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry scale={ 1.5 } />
-            <meshBasicMaterial color='mediumpurple' />
-        </mesh>
+        <group ref={ cubeRef }>
+            <mesh rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
+                <boxGeometry scale={ 1.5 } />
+                <meshBasicMaterial color='mediumpurple' />
+            </mesh>
 
-        <mesh position-x={ -2 } scale={ 1 }>
-            <sphereGeometry />
-            <meshBasicMaterial color='orange'/>
-        </mesh>
+            <mesh position-x={ -2 } scale={ 1 }>
+                <sphereGeometry />
+                <meshBasicMaterial color='orange'/>
+            </mesh>
+        </group>
 
         <mesh rotation-x={Math.PI / 2} position-y={ -2 } scale={ 10 }>
             <planeGeometry />
